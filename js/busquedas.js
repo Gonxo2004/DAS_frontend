69b9865b1dc60b124101f5a2ebf8ef1adbfdea87
LuchitoGarcia <luisgar2004@gmail.com>
2025-02-06 18:45:54 +0100

    Mejorar la función de puja para validar la cantidad mínima antes de realizar la puja

--- js/busquedas.js
+++ js/busquedas.js
@@ -3,7 +3,16 @@ function changeImage(newSrc) {
     mainImage.src = newSrc;
   }
 
-function bidMessage(){
+function bidMessage(minimunBid , currentBid){ 
     bidMessage_text = document.getElementById("bid_message");
+
+    if (minimunBid > currentBid) {
+        bidMessage_text.textContent = "La puja debe ser mayor a " + minimunBid + "€";
+        bidMessage_text.style.color = "red";
+        return;
+    }
+    
     bidMessage_text.textContent = "Puja realizada con exito!!";
+    bidMessage_text.style.color = "green";
+  
 }
